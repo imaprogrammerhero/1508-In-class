bf7de2b31958434757e6925b6f57beb7979e3762
imaprogrammerhero <54901644+imaprogrammerhero@users.noreply.github.com>
2019-09-27 09:43:12 -0600

    SQL

---
+++ DDL/Create Database.sql
@@ -0,0 +1,9 @@
+--A single line comment is one that starts with two dashes
+--CREATE DATABASE MyDatabase
+--GO -- Used to indicate that the above code should run as a "batch"
+--DROP DATABASE MyDatabase
+--GO
+
+--[Ctrl]+r-toggles messeages/results window
+USE MyDatabase -- change the execution conetxt for the script that follows
+GO -- run a batch
